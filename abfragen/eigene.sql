74cedac202dcbad7cb6d496ba63c08fadb9d6bf8
Jan200101 <sentrycraft123@gmail.com>
2022-03-10 14:11:08 +0100

    update abfragen

---
+++ abfragen/eigene.sql
@@ -0,0 +1,11 @@
+.mode table
+
+SELECT
+	strftime("%Y-%m-%d", datetime) as short_date,
+	MIN(tempwert),
+	MAX(tempwert),
+	AVG(tempwert)
+FROM temperaturUndLuftdruck
+GROUP BY short_date
+ORDER BY short_date DESC
+LIMIT 10;
--- abfragen/temperaturabfrage.sql
+++ abfragen/temperaturabfrage.sql
@@ -1,7 +1,10 @@
+.mode table
+
 SELECT
     MIN(tempwert) as "Maximal Temperatur",
     MAX(tempwert) as "Minimal Temperatur",
     AVG(tempwert) as "Durschnittstemperatur"
 FROM temperaturUndLuftdruck
 WHERE
-    strftime('%m.%d', datetime) = '03.14'
+    strftime('%m.%d', datetime) = '03.14' AND
+    strftime('%Y', datetime) = strftime('%Y', DATE('NOW', '-1 year'));
